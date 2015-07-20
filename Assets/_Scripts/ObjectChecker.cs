a605353204d0c2ceef78b594ca473c3e3e2f895b
Ramses di Perna <15826@ma-web.nl>
2015-07-20 20:46:55 +0200

    Made a component that checks if the object is seen by the given camera he needs to check It triggers events that you can use to activate other functions

---
+++ Assets/_Scripts/ObjectChecker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectChecker : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		GameObject.Find ("Cube").GetComponent<CameraSeeTriggerObject> ().OnCameraEnter += DoEnter; //This is for debugging! 
+		GameObject.Find ("Cube").GetComponent<CameraSeeTriggerObject> ().OnCameraStay += DoStay; //This is for debugging! 
+		GameObject.Find ("Cube").GetComponent<CameraSeeTriggerObject> ().OnCameraExit += DoExit; //This is for debugging! 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject.Find ("Cube").GetComponent<CameraSeeTriggerObject> ().CheckSeenByCamera (GameObject.Find ("CameraManCamera").GetComponent<Camera>()); //This is for debugging! 
+
+
+	}
+
+	void DoEnter(Camera cam){
+		Debug.Log ("Enter " + cam);
+	}
+	void DoStay(Camera cam){
+		Debug.Log ("Stay " + cam);
+	}
+
+	void DoExit(Camera cam){
+		Debug.Log ("Exit " + cam);
+	}
+}
