58c540303953810b297d266538cc1ced4774e6b9
Ramses di Perna <15826@ma-web.nl>
2015-07-20 14:26:52 +0200

    First Commit

---
+++ Assets/_Scripts/Utils/DualScreenResolutionMod.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.SetResolution(3840, 1080,false);
+	}
+}
