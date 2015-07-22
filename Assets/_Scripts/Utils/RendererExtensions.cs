13559b850f15a8062e10cfdc8e559e4b8615fff7
M.W.N.M. Janssen <mwnm.janssen@student.fontys.nl>
2015-07-22 16:32:18 +0200

    RendererExtensions.cs method toegevoegd om width te krijgen

--- Assets/_Scripts/Utils/RendererExtensions.cs
+++ Assets/_Scripts/Utils/RendererExtensions.cs
@@ -1,12 +1,250 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RendererExtensions : MonoBehaviour {
 
-	public static bool IsVisibleFrom(Renderer renderer, Camera camera)
+	public static bool IsVisibleFrom(Renderer renderer, Camera camera, Collider collider)
 	{
+        //Code for checking whether the object is rendered within the "Bounding box" of the camera
+
 		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
-		Debug.Log (planes);
-		return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+		//Debug.Log (planes);
+		//return 
+        bool canBeVisible = GeometryUtility.TestPlanesAABB(planes, collider.bounds);
+        
+        if (!canBeVisible) return false;
+        //Code for checking whether the objects corners are visible from the camera perspective. So not blocked by any other objects
+        Bounds objBounds = collider.bounds;
+        float adjustValue = 1;
+        float xNeg = objBounds.center.x - (objBounds.extents.x * adjustValue);
+        float xPos = objBounds.center.x + (objBounds.extents.x * adjustValue);
+        float yNeg = objBounds.center.y - (objBounds.extents.y * adjustValue);
+        float yPos = objBounds.center.y + (objBounds.extents.y * adjustValue); 
+        float zNeg = objBounds.center.z - (objBounds.extents.z * adjustValue);
+        float zPos = objBounds.center.z + (objBounds.extents.z * adjustValue);
+
+        //List all different coordinates of the cube vertexes
+        List<Vector3> cornerCoordinates = new List<Vector3>();
+        for (int x = 0; x < 2; x++)
+        {
+            for (int y = 0; y < 2; y++)
+            {
+                for (int z = 0; z < 2; z++)
+                {
+                    Vector3 coor = new Vector3();
+                    coor.x = (x == 0) ? xNeg : xPos;
+                    coor.y = (y == 0) ? yNeg : yPos;
+                    coor.z = (z == 0) ? zNeg : zPos;
+                    cornerCoordinates.Add(coor);
+                }
+            }
+        }
+
+        //Cast a raycast from the camera to the vertex and see if it hits the actual object
+        int hits = 0;
+
+
+        foreach (Vector3 coor in cornerCoordinates)
+        {
+            Vector3 newCoor = coor;
+            float frustatedModifier = 0.9f;
+            //Adapt the coor to the field of view? approximately
+            float distance = Vector3.Distance(objBounds.center, camera.transform.position);  // afstand tussen jou en het object
+            float frustumHeight = 2 * distance * Mathf.Tan(camera.fieldOfView * 0.5f * Mathf.Deg2Rad) * frustatedModifier; // hoogte van je view
+            float bottomFrustumHeight = camera.transform.position.y - (frustumHeight / 2) + (Mathf.Tan(Mathf.Deg2Rad * (360 - camera.transform.eulerAngles.x)) * distance); // bodem van je view tot het object
+            float topFrustumHeight = frustumHeight + bottomFrustumHeight;
+
+            float frustumWidth = frustumHeight * camera.aspect;
+            //float leftFrustumWidth = camera.transform.position.y - (frustumWidth / 2) + (Mathf.Tan(Mathf.Deg2Rad * (360 - camera.transform.eulerAngles.x)) * distance);
+            //float rightFrustumWidth = frustumWidth + leftFrustumWidth;
+
+            //Debug.Log(topFrustumHeight);
+            //Debug.Log(bottomFrustumHeight);
+            
+            if (coor.y == yPos)
+            {
+                if (coor.y >= topFrustumHeight)
+                {
+                    newCoor.y = topFrustumHeight;
+                }
+            }
+            else if(coor.y == yNeg)
+            {
+                if (coor.y <= bottomFrustumHeight)
+                {
+                    newCoor.y = bottomFrustumHeight;
+                }
+            }
+
+            //int layerMask = (int)(1 << 8);
+            RaycastHit hitInfo = new RaycastHit();
+            
+
+            if (Physics.Raycast(camera.transform.position, newCoor - camera.transform.position, out hitInfo, 1000))
+            {
+                
+                //Debug.Log(collider.gameObject);
+                if (hitInfo.collider.gameObject.Equals(collider.gameObject))
+                {
+                    Debug.DrawRay(camera.transform.position, newCoor - camera.transform.position, Color.green, 0.5f);
+                    //Debug.Log(hitInfo.collider.gameObject + " Coor: " + newCoor);
+                    hits++;
+                    GetPerspectiveWidth(collider, camera);
+                }
+            }
+        }
+        //Debug.Log(hits);
+        //For now, 1+ hits means it is visible
+        return (hits > 0);
 	}
+
+    public static float GetPerspectiveWidth(Collider objectCollider, Camera camera)
+    {
+        //First get the center middle points
+        Bounds objBounds = objectCollider.bounds;
+        float adjustValue = 1f;
+        float xNeg = objBounds.center.x - (objBounds.extents.x * adjustValue);
+        float xPos = objBounds.center.x + (objBounds.extents.x * adjustValue);
+
+        float zNeg = objBounds.center.z - (objBounds.extents.z * adjustValue);
+        float zPos = objBounds.center.z + (objBounds.extents.z * adjustValue);
+
+        List<Vector3> cornerCoordinates = new List<Vector3>();
+        for (int x = 0; x < 2; x++)
+        {
+                for (int z = 0; z < 2; z++)
+                {
+                    Vector3 coor = new Vector3();
+                    coor.x = (x == 0) ? xNeg : xPos;
+                    coor.y = objBounds.center.y;
+                    coor.z = (z == 0) ? zNeg : zPos;
+                    cornerCoordinates.Add(coor);
+                }
+        }
+
+        Vector3 mostLeft = camera.WorldToViewportPoint(cornerCoordinates[0]);
+        Vector3 mostRight = camera.WorldToViewportPoint(cornerCoordinates[0]);
+
+        Vector3 originalLeft = cornerCoordinates[0];
+        Vector3 originalRight = cornerCoordinates[0];
+
+        //Debug.Log("Center: " + mostLeft);
+
+        foreach (Vector3 coor in cornerCoordinates)
+        {
+
+            Vector3 screenCoor = camera.WorldToViewportPoint(coor);
+            if (screenCoor.x <= mostLeft.x)
+            {
+                mostLeft = camera.WorldToViewportPoint(coor);
+                originalLeft = coor;
+            }
+            else if (screenCoor.x >= mostRight.x)
+            {
+                mostRight = camera.WorldToViewportPoint(coor);
+                originalRight = coor;
+            }
+            //Debug.Log(screenCoor);
+        }
+        
+        //Fix for weird thing with only seeing one plane (ask me, or not. Rather dont actually)
+        Vector3 firstClosest = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
+        Vector3 secondClosest = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
+        Vector3 thirdClosest = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
+        if (originalLeft.x == originalRight.x || originalRight.z == originalLeft.z)
+        {
+            
+            foreach (Vector3 coor in cornerCoordinates)
+            {
+                if (Vector3.Distance(camera.transform.position, thirdClosest) >= Vector3.Distance(camera.transform.position, coor))
+                {
+                    thirdClosest = coor;
+                    if (Vector3.Distance(camera.transform.position, secondClosest) >= Vector3.Distance(camera.transform.position, thirdClosest))
+                    {
+                        Vector3 placeholder = secondClosest;
+                        secondClosest = thirdClosest;
+                        thirdClosest = placeholder;
+                        if (Vector3.Distance(camera.transform.position, firstClosest) >= Vector3.Distance(camera.transform.position, secondClosest))
+                        {
+                            Vector3 placeholder2 = firstClosest;
+                            firstClosest = secondClosest;
+                            secondClosest = placeholder2;
+                        }
+                    }
+                }
+            }
+
+            //Vector3
+
+            if (Mathf.Abs(objBounds.center.x - camera.transform.position.x) <= Mathf.Abs(objBounds.center.x - firstClosest.x))
+            {
+                if (firstClosest.z == thirdClosest.z)
+                {
+                    secondClosest = thirdClosest;
+                }
+            }
+            else
+            {
+                //dan is ie in de z categorie
+                if (firstClosest.x == thirdClosest.x)
+                {
+                    secondClosest = thirdClosest;
+                }
+            }
+
+
+            if (camera.WorldToViewportPoint(firstClosest).x >= camera.WorldToViewportPoint(secondClosest).x)
+            {
+                originalLeft = secondClosest;
+                originalRight = firstClosest;
+            }
+            else
+            {
+                originalLeft = firstClosest;
+                originalRight = secondClosest;
+            }
+
+            mostLeft = camera.WorldToViewportPoint(originalLeft);
+            mostRight = camera.WorldToViewportPoint(originalRight);
+
+            Debug.Log("weird");
+        }
+        //TODO fix bug with the viewpoint switching and the code below is not correct anymore somehow
+        
+
+        Debug.Log(camera.ViewportToScreenPoint(mostRight));
+        Debug.Log("left" + camera.ViewportToScreenPoint(mostLeft));
+
+        if (mostLeft.x <= 0)
+        {
+            mostLeft.x = 0;
+        }
+        else if (mostLeft.x >= 1)
+        {
+            mostLeft.x = 1;
+        }
+
+        if (mostRight.x <= 0)
+        {
+            mostRight.x = 0;
+        }
+        else if (mostRight.x >= 1)
+        {
+            mostRight.x = 1;
+        }
+
+
+        Debug.DrawLine(originalLeft, objBounds.center, Color.blue, 0.5f);
+        Debug.DrawLine(originalRight, objBounds.center, Color.red, 0.5f);
+        
+        Debug.Log(camera.pixelRect);
+        
+        float width = camera.ViewportToScreenPoint(mostRight).x - camera.ViewportToScreenPoint(mostLeft).x;
+        Debug.Log(width);
+        return width;  //Total width can be gotten with pixelwidth of camera.
+
+        // also return x and y from center point of visible points!
+        //maybe also inform of pixel rect
+    }
 }
